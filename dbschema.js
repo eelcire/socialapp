85bd0ed2f2384f6136a71f313cfe09b3c18d3888
eelcire <eelcire.eelcire@hotmail.com>
2020-01-02 17:27:17 -0600

    schema

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    posts: [
+        {
+            userHandle: 'user',
+            body: 'this is a post body',
+            createdAt: '2019-03-15T11:46:01.018Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
