c871cb6d5788e6c4c3afe40724d2441896c7eb29
eelcire <eelcire.eelcire@hotmail.com>
2020-01-06 17:38:46 -0600

    getting and commenting on post

--- functions/index.js
+++ functions/index.js
@@ -2,17 +2,20 @@ const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/fbAuth');
 
-const { getAllPosts, postOnePost } = require('./handlers/posts');
+const { getAllPosts, postOnePost, getPost, commentOnPost } = require('./handlers/posts');
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
 
 // Post routes
 app.get('/posts', getAllPosts);
 app.post('/post', FBAuth, postOnePost);
-app.post('/user/image', FBAuth, uploadImage)
-app.post('/user', FBAuth, addUserDetails)
-app.get('/user', FBAuth, getAuthenticatedUser)
+app.get('/post/:postId', getPost)
+app.post('/post/:postId/comment', FBAuth, commentOnPost)
+
 //User routes
 app.post('/signup', signup)
 app.post('/login', login)
+app.post('/user/image', FBAuth, uploadImage)
+app.post('/user', FBAuth, addUserDetails)
+app.get('/user', FBAuth, getAuthenticatedUser)
 
 exports.api = functions.https.onRequest(app);
